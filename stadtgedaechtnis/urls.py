cd147a1752c1b14d552620b74c99b79642cd6e6b
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-02-26 12:05:25 +0100

    Fix typo in stadtgedaechtnis urls

--- stadtgedaechtnis/urls.py
+++ stadtgedaechtnis/urls.py
@@ -8,5 +8,5 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    url(r'^$'), TemplateView.as_view(
-        template_name = "index.html"))
+    url(r'^$', TemplateView.as_view(
+        template_name = "index.html")))
