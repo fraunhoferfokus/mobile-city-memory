8f41b53071a8c4ecf5beb4754a2b87279d5a7be2
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-03-04 15:56:18 +0100

    Changes to prototype and css

--- stadtgedaechtnis/urls.py
+++ stadtgedaechtnis/urls.py
@@ -4,9 +4,9 @@ Created on 26.02.2014
 @author: jpi
 '''
 
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(
-        template_name = 'stadtgedaechtnis/index.html')))
+    url(r'^$', TemplateView.as_view(template_name = 'stadtgedaechtnis/index.html')),
+    url(r'^i18n/', include('django.conf.urls.i18n')),)
