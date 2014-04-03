665fbf6fca7748d64454d6c12e51864e4c8349da
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-04-03 19:05:47 +0200

    Initial web services package

--- stadtgedaechtnis/services/urls.py
+++ stadtgedaechtnis/services/urls.py
@@ -1,1 +1,12 @@
 __author__ = 'jpi'
+
+from django.conf.urls import patterns, url
+
+from stadtgedaechtnis.services.views import *
+
+
+urlpatterns = patterns('',
+    # TODO: add a mixin for localhost access only
+    # TODO: add parameters
+    url(r'^get-nearby-places/$', GetNearbyPlacesDBPedia.as_view(), name="get-nearby-places"),
+    )
--- stadtgedaechtnis/services/views.py
+++ stadtgedaechtnis/services/views.py
@@ -1,1 +1,17 @@
 __author__ = 'jpi'
+
+from django.views.generic import View
+from django.http import HttpResponse
+
+
+class GetNearbyPlacesDBPedia(View):
+
+    lat = None
+    lon = None
+
+    def get(self, request, *args, **kwargs):
+        self.lat
+        return HttpResponse("",
+                            content_type="application/json")
+
+
