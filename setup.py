1b868af3b155e6b06ef18596098c718782ce6881
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-04-04 15:23:56 +0200

    Setup files

---
+++ setup.py
@@ -0,0 +1,34 @@
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+	name = "mobile-city-history",
+	version = 0.1,
+	author = "Jan-Christopher Pien",
+	author_email = "jan_christopher.pien@fokus.fraunhofer.de",
+	url = "http://www.foo.bar",
+	license = "MIT",
+	description = "A mobile city history to enable citizens to explore the history of their surroundings.",
+	packages = find_packages(),
+	zip_safe = False,
+	include_package_data = True,
+
+	install_requires = [
+		"sparqlwrapper",
+		"django >= 1.6"
+		],
+
+	classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Other Audience",
+        "Framework :: Django",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary",
+    ],
+
+	)
