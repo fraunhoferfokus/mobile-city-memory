691859b5346b53e88043239f50f7c6d82f1f6656
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-04-08 12:50:48 +0200

    Update dependencies

--- setup.py
+++ setup.py
@@ -4,23 +4,25 @@ ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 
 setup(
-	name = "mobile-city-history",
-	version = 0.1,
-	author = "Jan-Christopher Pien",
-	author_email = "jan_christopher.pien@fokus.fraunhofer.de",
-	url = "http://www.foo.bar",
-	license = "MIT",
-	description = "A mobile city history to enable citizens to explore the history of their surroundings.",
-	packages = find_packages(),
-	zip_safe = False,
-	include_package_data = True,
+    name = "mobile-city-history",
+    version = 0.1,
+    author = "Jan-Christopher Pien",
+    author_email = "jan_christopher.pien@fokus.fraunhofer.de",
+    url = "http://www.foo.bar",
+    license = "MIT",
+    description = "A mobile city history to enable citizens to explore the history of their surroundings.",
+    packages = find_packages(),
+    zip_safe = False,
+    include_package_data = True,
 
-	install_requires = [
-		"sparqlwrapper",
-		"django >= 1.6"
-		],
+    install_requires = [
+        "sparqlwrapper",
+        "django >= 1.6",
+        "jsonpickle >= 0.7.0",
+        "django-apptemplates",
+        ],
 
-	classifiers = [
+    classifiers = [
         "Programming Language :: Python",
         "Development Status :: 2 - Pre-Alpha",
         "Environment :: Web Environment",
@@ -31,4 +33,4 @@ setup(
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary",
     ],
 
-	)
+    )
