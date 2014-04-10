1e4bccab2911cce65ebe1031393843e76fd24839
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-04-10 17:51:15 +0200

    Update media directory

--- Mobiles_Stadtgedaechtnis/urls.py
+++ Mobiles_Stadtgedaechtnis/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 import stadtgedaechtnis.admin
+import settings
 
 js_info_dict = {
     'packages': ('stadtgedaechtnis',),
@@ -11,4 +12,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(stadtgedaechtnis.admin.site.urls)),
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^jsi18n/', 'django.views.i18n.javascript_catalog', js_info_dict),
-)
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, }),
+    )
