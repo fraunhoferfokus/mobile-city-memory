2c190d367be48cbe2f1e27f787776fec19822996
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-05-08 13:41:14 +0200

    Add django-mssql

--- Mobiles_Stadtgedaechtnis/settings.py
+++ Mobiles_Stadtgedaechtnis/settings.py
@@ -68,7 +68,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'C:/Users/portal/Server/static'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
