2af696784cb8ea01028fdca1c806b462e6d1942b
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-05-08 14:29:11 +0200

    Different provider

--- Mobiles_Stadtgedaechtnis/settings.py
+++ Mobiles_Stadtgedaechtnis/settings.py
@@ -21,6 +21,7 @@ DATABASES = {
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'provider':'SQLCLI11',
     }
 }
 
