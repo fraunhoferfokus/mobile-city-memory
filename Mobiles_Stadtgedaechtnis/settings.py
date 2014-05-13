a25ad9a537d490413d9aa9f5b4a6b2b5e6c787e1
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-05-13 15:47:28 +0200

    Change provider to SQLNCLI11

--- Mobiles_Stadtgedaechtnis/settings.py
+++ Mobiles_Stadtgedaechtnis/settings.py
@@ -22,7 +22,7 @@ DATABASES = {
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
         'OPTIONS': {
-            'provider': 'SQLOLEDB',
+            'provider': 'SQLNCLI11',
             'use_mars': False
         }
     }
