26c8349a6be2214a1098f7349ddb26ac96ab9004
Jan-Christopher Pien <jan_pien@gmx.de>
2014-05-21 17:14:02 +0200

    Delete local_settings.py

--- Mobiles_Stadtgedaechtnis/local_settings.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'jpi'
-
-# User settings, not Django settings from here
-GOOGLE_API_KEY = 'AIzaSyBu_pyZCIn6bsQz6NtcuCYW8TtwITydf_I' # for testing only!
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'city-memory2=@i6q_lt(n(hgmnzw1s0a7$u!chj0dirp!5uck=ags=j@)k-a'
