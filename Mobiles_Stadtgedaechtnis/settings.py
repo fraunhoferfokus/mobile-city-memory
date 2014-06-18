372ebbf93accd069b913146bc4275b66a052ebee
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-06-18 19:14:52 +0200

    New settings

--- Mobiles_Stadtgedaechtnis/settings.py
+++ Mobiles_Stadtgedaechtnis/settings.py
@@ -98,10 +98,10 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    'stadtgedaechtnis_backend.services.authentication.middleware.TokenSessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.gzip.GZipMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
