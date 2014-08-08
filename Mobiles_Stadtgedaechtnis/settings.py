0a0f1eb27bbe1f672546c719b379cb42254963d6
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-08-08 13:04:27 +0200

    Enable csrf middleware

--- Mobiles_Stadtgedaechtnis/settings.py
+++ Mobiles_Stadtgedaechtnis/settings.py
@@ -102,7 +102,7 @@ MIDDLEWARE_CLASSES = (
     'stadtgedaechtnis_backend.services.authentication.middleware.TokenSessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.gzip.GZipMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
