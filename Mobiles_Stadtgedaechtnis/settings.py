a262d0e606e82c87e7866b4c545781a4adba3f62
Jan-Christopher Pien <jan-christopher.pien@fokus.fraunhofer.de>
2014-11-11 13:20:55 +0100

    Add port to mysql

--- Mobiles_Stadtgedaechtnis/settings.py
+++ Mobiles_Stadtgedaechtnis/settings.py
@@ -20,7 +20,7 @@ DATABASES = {
         'USER': 'city-memory',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
